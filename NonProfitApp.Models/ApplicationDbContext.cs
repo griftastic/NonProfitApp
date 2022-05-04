658bb0d1c565aa23dfdc0d3b5f734eb082808a20
jonesarn <ajones10196@icloud.com>
2022-05-03 20:53:56 -0400

    I think this is the DBContext

--- NonProfitApp.Models/ApplicationDbContext.cs
+++ NonProfitApp.Models/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using NonProfitApp.Data.Entities;
 
 namespace NonProfitApp.Models
 {
     public class ApplicationDbContext : DbContext
     {
-        
+        public DbSet <UserEntity> Volunteer {get;set;}
     }
 }
