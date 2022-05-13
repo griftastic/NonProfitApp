80565cd50724a831c0641c133af7ba78d6022e8f
Griffin Brown <browgrif@gmail.com>
2022-05-12 21:07:46 -0400

    Armond and Griffin are on our  Authorization BS

--- NonProfitApp.WebAPI/Controllers/UserController.cs
+++ NonProfitApp.WebAPI/Controllers/UserController.cs
@@ -8,6 +8,8 @@ using NonProfitApp.Services.User;
 using Microsoft.AspNetCore.Authorization;
 using NonProfitApp.Models.User;
 using NonProfitApp.Data.Entities;
+using NonProfitApp.Models.Token;
+using NonProfitApp.Services.Token;
 
 namespace NonProfitApp.WebAPI.Controllers
 {
@@ -16,9 +18,11 @@ namespace NonProfitApp.WebAPI.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
-        public UserController(IUserService userService)
+        private readonly ITokenService _tokenService;
+        public UserController(IUserService userService, ITokenService tokenService)
         {
             _userService = userService;
+            _tokenService = tokenService;
         }
 
         [HttpPost("Register")]
@@ -36,7 +40,7 @@ namespace NonProfitApp.WebAPI.Controllers
             return BadRequest("User could not be registered.");
         }
         
-        [HttpGet("{userId:int}")]
+        [Authorize, HttpGet("{userId:int}")]
         public async Task<IActionResult> GetById([FromRoute] int userId)
         {
             var userDetail = await _userService.GetUserByIdAsync(userId);
@@ -46,6 +50,18 @@ namespace NonProfitApp.WebAPI.Controllers
                 return NotFound();
             }
             return Ok(userDetail);
+        }
+
+        [HttpPost("~/api/Token")]
+        public async Task<IActionResult> Token([FromBody] TokenRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var tokenResponse = await _tokenService.GetTokenAsync(request);
+            if (tokenResponse is null)
+                return BadRequest("Invalid username or password.");
+
+            return Ok(tokenResponse);
         }
     }
 }
